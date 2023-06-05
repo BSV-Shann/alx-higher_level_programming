48ce8c55d8f0bfb8b200ca6e5f5486526e49e9d2
BenieShann <shann.vana@gmail.com>
2023-06-05 16:27:17 +0300

    script that runs Python code

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -4,12 +4,11 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list
+ * struct listint_s - singly linked list.
  * @n: integer
- * @next: points to the next node
+ * @next: points to the next node.
  *
- * Description: singly linked list node structure
- * for Holberton project
+ * Description:singly linked list node structure.
  */
 
 typedef struct listint_s
